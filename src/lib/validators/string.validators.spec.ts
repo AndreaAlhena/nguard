f557e78a71ebf3a33b0816f646b9e43857a228bd
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-16 19:56:10 +0100

    AlphaDash coded and tested

--- src/lib/validators/string.validators.spec.ts
+++ src/lib/validators/string.validators.spec.ts
@@ -2,9 +2,9 @@ import { AbstractControl } from "@angular/forms";
 import { StringValidators } from "./string.validators";
 import { createAbstractControlSpy } from "../utils/test-utils";
 
-describe('String Validators', () => {
-    let control: jasmine.SpyObj<AbstractControl>;
+let control: jasmine.SpyObj<AbstractControl>;
 
+describe('String Validators - Alpha', () => {
     it('Alpha - Valid string (ASCII only)', () => {
         control = createAbstractControlSpy('abcABC');
         expect(StringValidators.alpha(true)(control)).toBeNull();
@@ -73,5 +73,67 @@ describe('String Validators', () => {
     it('Alpha - Invalid string should fail (symbol)', () => {
         control = createAbstractControlSpy('%');
         expect(StringValidators.alpha()(control)).toEqual({alpha: true});
+    });
+});
+
+describe('String Validators - AlphaDash', () => {
+    it('AlphaDash - Valid string (ASCII only)', () => {
+        control = createAbstractControlSpy('abcABC_-123');
+        expect(StringValidators.alphaDash(true)(control)).toBeNull();
+    });
+
+    it('AlphaDash - Invalid string should fail (ASCII only / White space in the middle)', () => {
+        control = createAbstractControlSpy('abc ABC');
+        expect(StringValidators.alphaDash(true)(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Invalid string should fail (ASCII only / White space at the beginning)', () => {
+        control = createAbstractControlSpy(' abcABC');
+        expect(StringValidators.alphaDash(true)(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Invalid string should fail (ASCII only / White space at the end)', () => {
+        control = createAbstractControlSpy('abcABC ');
+        expect(StringValidators.alphaDash(true)(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Invalid string should fail (ASCII only / japanese character)', () => {
+        control = createAbstractControlSpy('字');
+        expect(StringValidators.alphaDash(true)(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Invalid string should fail (ASCII only / cyrillic character)', () => {
+        control = createAbstractControlSpy('Б');
+        expect(StringValidators.alphaDash(true)(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Invalid string should fail (ASCII only / symbol)', () => {
+        control = createAbstractControlSpy('%');
+        expect(StringValidators.alphaDash(true)(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Valid string', () => {
+        control = createAbstractControlSpy('abc字Б');
+        expect(StringValidators.alphaDash()(control)).toBeNull();
+    });
+
+    it('AlphaDash - Invalid string should fail (White space in the middle)', () => {
+        control = createAbstractControlSpy('abc ABC');
+        expect(StringValidators.alphaDash()(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Invalid string should fail (White space at the beginning)', () => {
+        control = createAbstractControlSpy(' abcABC');
+        expect(StringValidators.alphaDash()(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Invalid string should fail (White space at the end)', () => {
+        control = createAbstractControlSpy('abcABC ');
+        expect(StringValidators.alphaDash()(control)).toEqual({alphaDash: true});
+    });
+
+    it('AlphaDash - Invalid string should fail (symbol)', () => {
+        control = createAbstractControlSpy('%');
+        expect(StringValidators.alphaDash()(control)).toEqual({alphaDash: true});
     });
 });
--- src/lib/validators/string.validators.ts
+++ src/lib/validators/string.validators.ts
@@ -2,7 +2,7 @@ import { AbstractControl } from "@angular/forms";
 
 export namespace StringValidators {
     /**
-     * Validate an attribute that contains only Unicode alphabetic characters (matched by \p{L} and \p{M})
+     * Validate that an attribute contains only Unicode alphabetic characters (matched by \p{L} and \p{M})
      * If the hasAsciiOnly flag is set to true, the validation is restricted by characters in the ASCII range
      * (a-z and A-Z)
      * 
@@ -20,11 +20,34 @@ export namespace StringValidators {
                 ? /^[a-zA-Z]+$/u
                 : /^[\p{L}\p{M}]+$/u;
 
-            if (!hasAsciiOnly) console.log(regEx.test(c.value));
-
             return regEx.test(c.value)
                 ? null
                 : { alpha: true };
+        };
+    };
+
+    /**
+     * Validate that an attribute contains only Unicode alphanumeric characters (matched by \p{L} and \p{M}), dash and underscores
+     * If the hasAsciiOnly flag is set to true, the validation is restricted by characters in the ASCII range
+     * (a-z, A-Z and 0-9), dashes and underscores
+     * 
+     * ```
+     * new FormControl('', [
+     *   StringValidators.alphaDash() // or StringValidators.alphaDash(true)
+     * ])
+     * ```
+     * @param {boolean} hasAsciiOnly If true, limits characters to ASCII chars (a-z and A-Z)
+     * @return {ValidationFn}
+     */
+    export const alphaDash = (hasAsciiOnly: boolean = false) => {
+        return (c: AbstractControl) => {
+            const regEx: RegExp = hasAsciiOnly
+                ? /^[a-zA-Z0-9_-]+$/u
+                : /^[\p{L}\p{M}N_-]+$/u;
+
+            return regEx.test(c.value)
+                ? null
+                : { alphaDash: true };
         };
     };
 }
