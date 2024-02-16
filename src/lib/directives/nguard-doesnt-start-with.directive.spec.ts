9b14aa4b5ca54fcaf3dc5806c71e7439a6c5c377
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-16 23:45:05 +0100

    Directive coded and tested

---
+++ src/lib/directives/nguard-doesnt-start-with.directive.spec.ts
@@ -0,0 +1,30 @@
+import { AbstractControl } from '@angular/forms';
+import { NguardDoesntStartWithDirective } from './nguard-doesnt-start-with.directive';
+import { createAbstractControlSpy } from '../utils/test-utils';
+
+describe('NguardDoesntStartWithDirective', () => {
+  let control: AbstractControl;
+  let directive: NguardDoesntStartWithDirective;
+
+  beforeEach(() => {
+    directive = new NguardDoesntStartWithDirective();
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  it('should validate a field that doesnt start with', () => {
+    control = createAbstractControlSpy('abcABC123');
+    directive.values = ['123', 'def'];
+
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should fail if the field starts with', () => {
+    control = createAbstractControlSpy('abcABC123');
+    directive.values = ['abc'];
+
+    expect(directive.validate(control)).toEqual({doesntStartWith: true});
+  });
+});
---
+++ src/lib/directives/nguard-doesnt-start-with.directive.ts
@@ -0,0 +1,22 @@
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { StringValidators } from '../validators/string.validators';
+
+@Directive({
+  providers: [{
+    multi: true,
+    provide: NG_VALIDATORS,
+    useExisting: NguardDoesntStartWithDirective
+  }],
+  selector: '[nguardDoesntStartWith]',
+  standalone: true
+})
+export class NguardDoesntStartWithDirective implements Validator {
+  @Input('nguardDoesntStartWith') public values!: string[];
+
+  constructor() { }
+
+  public validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    return StringValidators.doesntStartWith(...this.values)(control);
+  }
+}
