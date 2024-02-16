02e8b77fadb582c2859ba47dd4889256260df720
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-16 19:42:08 +0100

    Usage example

--- src/lib/validators/string.validators.ts
+++ src/lib/validators/string.validators.ts
@@ -5,7 +5,12 @@ export namespace StringValidators {
      * Validate an attribute that contains only Unicode alphabetic characters (matched by \p{L} and \p{M})
      * If the hasAsciiOnly flag is set to true, the validation is restricted by characters in the ASCII range
      * (a-z and A-Z)
-     *
+     * 
+     * ```
+     * new FormControl('', [
+     *   StringValidators.alpha() // or StringValidators.alpha(true)
+     * ])
+     * ```
      * @param {boolean} hasAsciiOnly If true, limits characters to ASCII chars (a-z and A-Z)
      * @return {ValidationFn}
      */
