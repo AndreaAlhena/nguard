582db1f3dcc0cccfdab0d97372c622d23ae2e52f
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-16 20:09:12 +0100

    AlphaDash directive with tests and interface

---
+++ src/lib/directives/nguard-alpha-dash.directive.spec.ts
@@ -0,0 +1,43 @@
+
+import { AbstractControl } from '@angular/forms';
+import { NguardAlphaDashDirective } from './nguard-alpha-dash.directive';
+import { createAbstractControlSpy } from '../utils/test-utils';
+
+describe('NguardAlphaDashDirective', () => {
+  let control: AbstractControl;
+  let directive: NguardAlphaDashDirective;
+
+  beforeEach(() => {
+    directive = new NguardAlphaDashDirective();
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  it('should validate a properly formatted value (ASCII true)', () => {
+    control = createAbstractControlSpy('abcABC_-123');
+    directive.config = {hasAsciiOnly: true};
+
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should validate a non properly formatted value (ASCII true)', () => {
+    control = createAbstractControlSpy('a 2 c');
+    directive.config = {hasAsciiOnly: true};
+
+    expect(directive.validate(control)).toEqual({alphaDash: true});
+  });
+
+  it('should validate a properly formatted value (ASCII false)', () => {
+    control = createAbstractControlSpy('abc字Б');
+
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should validate a non properly formatted value (ASCII false)', () => {
+    control = createAbstractControlSpy('a 2 c');
+
+    expect(directive.validate(control)).toEqual({alphaDash: true});
+  });
+});
---
+++ src/lib/directives/nguard-alpha-dash.directive.ts
@@ -0,0 +1,27 @@
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+// Interfaces
+import { IAlphaDashDirectiveConfig } from '../interfaces/alpha-dash-directive-config.interface';
+
+// Validators
+import { StringValidators } from '../validators/string.validators';
+
+@Directive({
+  providers: [{
+    multi: true,
+    provide: NG_VALIDATORS,
+    useExisting: NguardAlphaDashDirective
+  }],
+  selector: '[nguardAlphaDash]',
+  standalone: true
+})
+export class NguardAlphaDashDirective implements Validator {
+  @Input('nguardAlphaDash') public config!: IAlphaDashDirectiveConfig;
+
+  constructor() { }
+  
+  public validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    return StringValidators.alphaDash(this.config?.hasAsciiOnly)(control);
+  }
+}
