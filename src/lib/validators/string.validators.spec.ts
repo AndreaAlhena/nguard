8666f86973823fe7a58a6e11b75dbd054aea4e94
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-16 20:20:51 +0100

    alphaNum coded and tested

--- src/lib/validators/string.validators.spec.ts
+++ src/lib/validators/string.validators.spec.ts
@@ -113,7 +113,7 @@ describe('String Validators - AlphaDash', () => {
     });
 
     it('AlphaDash - Valid string', () => {
-        control = createAbstractControlSpy('abc字Б');
+        control = createAbstractControlSpy('abc字Б123');
         expect(StringValidators.alphaDash()(control)).toBeNull();
     });
 
@@ -135,5 +135,67 @@ describe('String Validators - AlphaDash', () => {
     it('AlphaDash - Invalid string should fail (symbol)', () => {
         control = createAbstractControlSpy('%');
         expect(StringValidators.alphaDash()(control)).toEqual({alphaDash: true});
+    });
+});
+
+describe('String Validators - AlphaNum', () => {
+    it('AlphaNum - Valid string (ASCII only)', () => {
+        control = createAbstractControlSpy('abcABC123');
+        expect(StringValidators.alphaNum(true)(control)).toBeNull();
+    });
+
+    it('AlphaNum - Invalid string should fail (ASCII only / White space in the middle)', () => {
+        control = createAbstractControlSpy('abc 123');
+        expect(StringValidators.alphaNum(true)(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Invalid string should fail (ASCII only / White space at the beginning)', () => {
+        control = createAbstractControlSpy(' abcABC');
+        expect(StringValidators.alphaNum(true)(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Invalid string should fail (ASCII only / White space at the end)', () => {
+        control = createAbstractControlSpy('abcABC ');
+        expect(StringValidators.alphaNum(true)(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Invalid string should fail (ASCII only / japanese character)', () => {
+        control = createAbstractControlSpy('字');
+        expect(StringValidators.alphaNum(true)(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Invalid string should fail (ASCII only / cyrillic character)', () => {
+        control = createAbstractControlSpy('Б');
+        expect(StringValidators.alphaNum(true)(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Invalid string should fail (ASCII only / symbol)', () => {
+        control = createAbstractControlSpy('%');
+        expect(StringValidators.alphaNum(true)(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Valid string', () => {
+        control = createAbstractControlSpy('abc字Б123');
+        expect(StringValidators.alphaNum()(control)).toBeNull();
+    });
+
+    it('AlphaNum - Invalid string should fail (White space in the middle)', () => {
+        control = createAbstractControlSpy('abc ABC');
+        expect(StringValidators.alphaNum()(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Invalid string should fail (White space at the beginning)', () => {
+        control = createAbstractControlSpy(' abcABC');
+        expect(StringValidators.alphaNum()(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Invalid string should fail (White space at the end)', () => {
+        control = createAbstractControlSpy('abcABC ');
+        expect(StringValidators.alphaNum()(control)).toEqual({alphaNum: true});
+    });
+
+    it('AlphaNum - Invalid string should fail (symbol)', () => {
+        control = createAbstractControlSpy('%');
+        expect(StringValidators.alphaNum()(control)).toEqual({alphaNum: true});
     });
 });
--- src/lib/validators/string.validators.ts
+++ src/lib/validators/string.validators.ts
@@ -43,11 +43,36 @@ export namespace StringValidators {
         return (c: AbstractControl) => {
             const regEx: RegExp = hasAsciiOnly
                 ? /^[a-zA-Z0-9_-]+$/u
-                : /^[\p{L}\p{M}N_-]+$/u;
+                : /^[\p{L}\p{M}\p{N}_-]+$/u;
 
             return regEx.test(c.value)
                 ? null
                 : { alphaDash: true };
+        };
+    };
+
+    /**
+     * Validate that an attribute contains only Unicode alphanumeric characters (matched by \p{L} and \p{M})
+     * If the hasAsciiOnly flag is set to true, the validation is restricted by characters in the ASCII range
+     * (a-z, A-Z and 0-9), dashes and underscores
+     * 
+     * ```
+     * new FormControl('', [
+     *   StringValidators.alphaDash() // or StringValidators.alphaDash(true)
+     * ])
+     * ```
+     * @param {boolean} hasAsciiOnly If true, limits characters to ASCII chars (a-z and A-Z)
+     * @return {ValidationFn}
+     */
+    export const alphaNum = (hasAsciiOnly: boolean = false) => {
+        return (c: AbstractControl) => {
+            const regEx: RegExp = hasAsciiOnly
+                ? /^[a-zA-Z0-9]+$/u
+                : /^[\p{L}\p{M}\p{N}]+$/u;
+
+            return regEx.test(c.value)
+                ? null
+                : { alphaNum: true };
         };
     };
 }
