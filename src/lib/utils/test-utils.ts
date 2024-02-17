5e688dda743716a3b7ea88d5fcb8130dd7a7b25b
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-17 18:10:13 +0100

    Utils: create control with sibling

--- src/lib/utils/test-utils.ts
+++ src/lib/utils/test-utils.ts
@@ -1,1 +1,16 @@
-export const createAbstractControlSpy = (value: string) => jasmine.createSpyObj('AbstractControl', {}, {value});
+export const createAbstractControlSpy = (value: string) => jasmine.createSpyObj('AbstractControl', {}, { value });
+
+export const createAbstractControlSpyWithSibling = (field1Value: string, field2Value: string) => {
+  const parent = jasmine.createSpyObj('FormGroup', ['get']);
+  const control1 = jasmine.createSpyObj('FormControl', [], {value: field1Value, parent});
+  const control2 = jasmine.createSpyObj('FormControl', [], {value: field2Value});
+
+  parent.get.and.returnValue(control2);
+
+  return control1;
+}
+// export const createGroupWithControls = (...controls: {[key: string]: any}[]) => jasmine.createSpyObj(
+//   'AbstractControl',
+//   {}, {
+//   controls: controls.map(c => ({[c.key]: c.value}))
+// }); 
