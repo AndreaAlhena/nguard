c8eae1b595a1d1ee15e86f808aec9045a1ad838e
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-17 21:56:33 +0100

    Lowercase directive coded and tested

---
+++ src/lib/directives/nguard-lowercase.directive.spec.ts
@@ -0,0 +1,26 @@
+import { AbstractControl } from '@angular/forms';
+import { createAbstractControlSpy } from '../utils/test-utils';
+import { NguardLowercaseDirective } from './nguard-lowercase.directive';
+
+describe('NguardLowercaseDirective', () => {
+  let control: AbstractControl;
+  let directive: NguardLowercaseDirective;
+
+  beforeEach(() => {
+    directive = new NguardLowercaseDirective();
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  it('should validate a lowercase field', () => {
+    control = createAbstractControlSpy('abc');
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should fail if the field is not lowercase', () => {
+    control = createAbstractControlSpy('ABC');
+    expect(directive.validate(control)).toEqual({lowercase: true});
+  });
+});
---
+++ src/lib/directives/nguard-lowercase.directive.ts
@@ -0,0 +1,22 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+// Validators
+import { StringValidators } from '../validators/string.validators';
+
+@Directive({
+  providers: [{
+    multi: true,
+    provide: NG_VALIDATORS,
+    useExisting: NguardLowercaseDirective
+  }],
+  selector: '[nguardLowercase]',
+  standalone: true
+})
+export class NguardLowercaseDirective implements Validator {
+  constructor() { }
+
+  public validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    return StringValidators.lowercase()(control);
+  }
+}
