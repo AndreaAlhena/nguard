892f0aad86066fc16eea12bfdb13e438fb3c64c9
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-19 18:29:09 +0100

    Any type accepted

--- src/lib/utils/test.utils.ts
+++ src/lib/utils/test.utils.ts
@@ -1,6 +1,6 @@
 export const createAbstractControlSpy = <T>(value: T) => jasmine.createSpyObj('AbstractControl', {}, { value });
 
-export const createAbstractControlSpyWithSibling = (field1Value: string, field2Value: string) => {
+export const createAbstractControlSpyWithSibling = (field1Value: any, field2Value: any) => {
   const parent = jasmine.createSpyObj('FormGroup', ['get']);
   const control1 = jasmine.createSpyObj('FormControl', [], {value: field1Value, parent});
   const control2 = jasmine.createSpyObj('FormControl', [], {value: field2Value});
