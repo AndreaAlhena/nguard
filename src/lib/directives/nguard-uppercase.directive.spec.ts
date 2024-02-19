7062b3c0b748b34c84ee3a814af5cd7c200944c4
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-19 16:02:30 +0100

    Uppercase directive coded and tested

---
+++ src/lib/directives/nguard-uppercase.directive.spec.ts
@@ -0,0 +1,26 @@
+import { AbstractControl } from '@angular/forms';
+import { createAbstractControlSpy } from '../utils/test-utils';
+import { NguardUppercaseDirective } from './nguard-uppercase.directive';
+
+describe('NguardUppercaseDirective', () => {
+  let control: AbstractControl;
+  let directive: NguardUppercaseDirective;
+
+  beforeEach(() => {
+    directive = new NguardUppercaseDirective();
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  it('should validate a lowercase field', () => {
+    control = createAbstractControlSpy('ABC');
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should fail if the field is not lowercase', () => {
+    control = createAbstractControlSpy('abc');
+    expect(directive.validate(control)).toEqual({uppercase: true});
+  });
+});
---
+++ src/lib/directives/nguard-uppercase.directive.ts
@@ -0,0 +1,22 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+
+// Validators
+import { StringValidators } from '../validators/string.validators';
+
+@Directive({
+  providers: [{
+    multi: true,
+    provide: NG_VALIDATORS,
+    useExisting: NguardUppercaseDirective
+  }],
+  selector: '[nguardUppercase]',
+  standalone: true
+})
+export class NguardUppercaseDirective implements Validator {
+  constructor() { }
+
+  public validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    return StringValidators.uppercase()(control);
+  }
+}
