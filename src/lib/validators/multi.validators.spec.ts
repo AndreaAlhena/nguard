e5ddda1fbfbc206d7db75f5286c36ae2ed7fdb1c
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-19 18:29:34 +0100

    Different validators tested

--- src/lib/validators/multi.validators.spec.ts
+++ src/lib/validators/multi.validators.spec.ts
@@ -7,6 +7,21 @@ let control: jasmine.SpyObj<AbstractControl>;
 describe('MultiValidators - Different', () => {
     it('Different Validator - Valid if values are different with same type (strict enabled)', () => {
         control = createAbstractControlSpyWithSibling('abc', 'def');
-        expect(MultiValidators.different('')(control)).toBeNull();
+        expect(MultiValidators.different('', true)(control)).toBeNull();
+    });
+
+    it('Different Validator - Valid if values are equal with different type (strict enabled)', () => {
+        control = createAbstractControlSpyWithSibling('1', 1);
+        expect(MultiValidators.different('', true)(control)).toBeNull();
+    });
+
+    it('Different Validator - Invalid if values are equal with same type (strict enabled)', () => {
+        control = createAbstractControlSpyWithSibling('abc', 'abc');
+        expect(MultiValidators.different('', true)(control)).toEqual({different: true});
+    });
+
+    it('Different Validator - Invalid if values are equal with different type (strict disabled)', () => {
+        control = createAbstractControlSpyWithSibling('1', 1);
+        expect(MultiValidators.different('')(control)).toEqual({different: true});
     });
 });
