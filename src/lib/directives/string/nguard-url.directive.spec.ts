12493522ff6ccb903190374d3cb1d317ee0a200b
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-20 20:32:36 +0100

    URL Directive coded and tested

---
+++ src/lib/directives/string/nguard-url.directive.spec.ts
@@ -0,0 +1,26 @@
+import { AbstractControl } from '@angular/forms';
+import { NguardUrlDirective } from './nguard-url.directive';
+import { createAbstractControlSpy } from '../../utils/test.utils';
+
+describe('NguardUrlDirective', () => {
+  let control: AbstractControl;
+  let directive: NguardUrlDirective
+
+  beforeEach(() => {
+    directive = new NguardUrlDirective();
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  it('should validate a proper url', () => {
+    control = createAbstractControlSpy('https://www.google.com');
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should invalidate a non properly formatted url', () => {
+    control = createAbstractControlSpy('');
+    expect(directive.validate(control)).toEqual({url: true});
+  });
+});
---
+++ src/lib/directives/string/nguard-url.directive.ts
@@ -0,0 +1,20 @@
+import { Directive } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { StringValidators } from '../../validators/string.validators';
+
+@Directive({
+  providers: [{
+    multi: true,
+    provide: NG_VALIDATORS,
+    useExisting: NguardUrlDirective
+  }],
+  selector: '[nguardUrl]',
+  standalone: true
+})
+export class NguardUrlDirective implements Validator {
+  constructor() { }
+
+  public validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    return StringValidators.url()(control);
+  }
+}
