545a1bf99c8473ba5fe431cecc3c28532b7bf1ea
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-21 14:28:33 +0100

    Public API exposes  directives

--- src/public-api.ts
+++ src/public-api.ts
@@ -2,4 +2,23 @@
  * Public API Surface of nguard
  */
 
+// Directives
+//   - multi
+export * from './lib/directives/multi/nguard-different.directive';
+export * from './lib/directives/multi/nguard-doesnt-end-with.directive';
+export * from './lib/directives/multi/nguard-doesnt-start-with.directive';
+export * from './lib/directives/multi/nguard-ends-with.directive';
+export * from './lib/directives/multi/nguard-required-if.directive';
+export * from './lib/directives/multi/nguard-same.directive';
+export * from './lib/directives/multi/nguard-starts-with.directive';
+//  - string
+export * from './lib/directives/string/nguard-alpha-dash.directive';
+export * from './lib/directives/string/nguard-alpha-num.directive';
+export * from './lib/directives/string/nguard-alpha.directive';
+export * from './lib/directives/string/nguard-ascii.directive';
+export * from './lib/directives/string/nguard-lowercase.directive';
+export * from './lib/directives/string/nguard-uppercase.directive';
+export * from './lib/directives/string/nguard-url.directive';
+
+// Validators
 export * from './lib/validators/nguard.validators';
