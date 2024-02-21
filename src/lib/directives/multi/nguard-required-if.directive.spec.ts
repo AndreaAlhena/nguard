2704bd12e07902946438db9ac98e8239dccec57a
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-02-21 14:00:00 +0100

    RequiredIf directive coded and tested

---
+++ src/lib/directives/multi/nguard-required-if.directive.spec.ts
@@ -0,0 +1,51 @@
+import { AbstractControl } from '@angular/forms';
+import { NguardRequiredIfDirective } from './nguard-required-if.directive';
+import { createAbstractControlSpyWithSibling } from '../../utils/test.utils';
+
+describe('NguardRequiredIfDirective', () => {
+  let control: AbstractControl;
+  let directive: NguardRequiredIfDirective;
+
+  beforeEach(() => {
+    directive = new NguardRequiredIfDirective();
+  });
+
+  it('should create an instance', () => {
+    expect(directive).toBeTruthy();
+  });
+
+  it('should validate if both the fields are set / config as string', () => {
+    control = createAbstractControlSpyWithSibling('value1', 'value2');
+    directive.config = 'fieldKey';
+
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should validate if both the fields are set / config as object - fieldKey only', () => {
+    control = createAbstractControlSpyWithSibling('value1', 'value2');
+    directive.config = {fieldKey: 'fieldKey'};
+
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should validate if both the fields are set / config as object - fieldKey and value', () => {
+    control = createAbstractControlSpyWithSibling('value1', 'value2');
+    directive.config = {fieldKey: 'fieldKey', value: 'value2'};
+
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should validate if both the fields are set / config as object - fieldKey and value, non strict', () => {
+    control = createAbstractControlSpyWithSibling('value1', '1');
+    directive.config = {fieldKey: 'fieldKey', value: 1};
+
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('should fail if both the fields are set / config as object - fieldKey and value of different types, strict', () => {
+    control = createAbstractControlSpyWithSibling('value1', '1');
+    directive.config = {fieldKey: 'fieldKey', isStrict: true, value: 1};
+
+    expect(directive.validate(control)).toEqual({requiredIf: true});
+  });
+});
---
+++ src/lib/directives/multi/nguard-required-if.directive.ts
@@ -0,0 +1,37 @@
+import { Directive, Input } from '@angular/core';
+import { AbstractControl, NG_VALIDATORS, ValidationErrors, Validator } from '@angular/forms';
+import { MultiValidators } from '../../validators/multi.validators';
+import { IRequiredIfConfig } from '../../interfaces/required-if-config.interface';
+import { primitive } from '../../utils/validators.utils';
+
+@Directive({
+  providers: [{
+    multi: true,
+    provide: NG_VALIDATORS,
+    useExisting: NguardRequiredIfDirective
+  }],
+  selector: '[nguardRequiredIf]',
+  standalone: true
+})
+export class NguardRequiredIfDirective implements Validator {
+  @Input('nguardRequiredIf') public config!: string | IRequiredIfConfig;
+
+  constructor() { }
+
+  public validate(control: AbstractControl<any, any>): ValidationErrors | null {
+    const fieldKey = typeof this.config === 'string'
+      ? this.config
+      : this.config.fieldKey;
+    
+    let
+      isStrict: boolean | undefined,
+      value: primitive | undefined;
+
+    if (typeof this.config === 'object') {
+      isStrict = this.config?.isStrict;
+      value = this.config?.value;
+    }
+
+    return MultiValidators.requiredIf(fieldKey, value, isStrict)(control);
+  }
+}
