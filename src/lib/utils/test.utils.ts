55b3e558612cdb0885f5980bacc69985343b753f
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2024-04-13 17:06:00 +0200

    Return type against createAbstractControlSpyWithSibling

--- src/lib/utils/test.utils.ts
+++ src/lib/utils/test.utils.ts
@@ -1,6 +1,8 @@
+import { AbstractControl } from "@angular/forms";
+
 export const createAbstractControlSpy = <T>(value: T) => jasmine.createSpyObj('AbstractControl', {}, { value });
 
-export const createAbstractControlSpyWithSibling = (field1Value: any, field2Value: any) => {
+export const createAbstractControlSpyWithSibling = (field1Value: any, field2Value: any): jasmine.SpyObj<AbstractControl> => {
   const parent = jasmine.createSpyObj('FormGroup', ['get']);
   const control1 = jasmine.createSpyObj('FormControl', [], {value: field1Value, parent});
   const control2 = jasmine.createSpyObj('FormControl', [], {value: field2Value});
