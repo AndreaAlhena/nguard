d362e3fffa15df43db321ba1c3b09a9fefb53d4a
David Piottante <d.piottante@nesea.it>
2024-04-26 19:13:40 +0200

    added test on range directive

--- src/lib/directives/number/nguard-range.directive.spec.ts
+++ src/lib/directives/number/nguard-range.directive.spec.ts
@@ -1,5 +1,6 @@
 import { AbstractControl } from "@angular/forms";
 import { NguardRangeDirective } from "./nguard-range.directive";
+import { createAbstractControlSpy } from "../../utils/test.utils";
 
 describe("NguardRangeDirective", () => {
   let control: AbstractControl;
@@ -8,9 +9,23 @@ describe("NguardRangeDirective", () => {
   beforeEach(() => {
     directive = new NguardRangeDirective();
   });
-  
+
   it("should create an instance", () => {
     const directive = new NguardRangeDirective();
     expect(directive).toBeTruthy();
+  });
+
+  it('Should validate a number in the given range', () => {
+    control = createAbstractControlSpy(5);
+    directive.values = [1, 5];
+
+    expect(directive.validate(control)).toBeNull();
+  });
+
+  it('Should validate a number is not in the given range', () => {
+    control = createAbstractControlSpy(7);
+    directive.values = [1, 5];
+
+    expect(directive.validate(control)).toEqual({range: true});
   });
 });
