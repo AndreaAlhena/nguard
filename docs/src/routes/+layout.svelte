30dc5e5196d99760458a7740833b3eff64976588
Andrea Alhena Tantimonaco <info@andreatantimonaco.me>
2025-12-21 16:45:17 +0100

    feat: integrate version switcher in layout

--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -1,6 +1,20 @@
 <!-- src/routes/+layout.svelte -->
-<script>
-    import '$lib/sass/global.sass';
+<script lang="ts">
+	import '$lib/sass/global.sass';
+	import VersionSwitcher from '../components/VersionSwitcher.svelte';
 </script>
 
+<div class="version-switcher-wrapper">
+	<VersionSwitcher />
+</div>
+
 <slot></slot>
+
+<style>
+	:global(.version-switcher-wrapper) {
+		position: fixed;
+		top: 20px;
+		right: 300px;
+		z-index: 1000;
+	}
+</style>
